49f108ce0e3365e8344324695017387def4011c3
Henrique Sanches Silva <87503412+HenriqueSanchesSilva@users.noreply.github.com>
2022-04-18 15:18:44 -0300

    js sintaxe basica

---
+++ Js/SintaxeBasica/Aula2 - Variaveis/JS/script.js
@@ -0,0 +1,15 @@
+//tipos primitivos de variaveis
+
+  //Booleanos = true or false
+  var vOrF = false;
+  console.log(typeof (vOrF));
+
+  //Number 
+  var number = 1;
+  console.log(typeof (number));
+
+  //string 
+  var nome = 'ricard';
+  console.log(typeof (nome));
+
+
