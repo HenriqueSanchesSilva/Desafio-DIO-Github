fd1f6d0759932a769069daf7541b755f44f07b84
Henrique Sanches Silva <87503412+HenriqueSanchesSilva@users.noreply.github.com>
2022-04-20 14:49:40 -0300

    atv modulos

---
+++ JavaScript/Modulos/JS/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Henrique", 18));
+console.log(mostraCidade("Henrique", "Guarulhos"));
+console.log(mostraHobby("Henrique", "Jogar Valorant"));
