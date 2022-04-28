fd26bb8829c2eec9ab24d4d477df5a893110b904
Henrique Sanches Silva <87503412+HenriqueSanchesSilva@users.noreply.github.com>
2022-04-28 11:35:23 -0300

    ty

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,28 +1,106 @@
-/* function soma(a: number, b:number){
+interface iPessoa{
+  nome: string;
+  idade: number;
+  nacionalidade;
+}
+
+interface Brasileiro extends Omit<iPessoa, 'nacionalidade'>{
+  
+}
+
+import $ from 'jquery';
+
+$.fn.extend({
+  novaFuncao(){
+    console.log("nova funcao");
+  }
+});
+
+$('body').novaFuncao();
+
+interface Cachorro{
+  nome: string;
+  idade: number;
+  parqueFav?: string;
+}
+
+type CachorroSomenteLeitura = {
+  +readonly [K in keyof Cachorro]-?: Cachorro[K]
+}
+
+class meuCachorro implements CachorroSomenteLeitura{
+  nome;
+  idade;
+  parqueFav;
+  constructor(nome, idade){
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const cao = new meuCachorro('Apolo', 14);
+
+
+interface iUsuario {
+  id: string;
+  email: string;
+  cargo?: 'funcionario' | 'gerente' | 'coordenador' | 'supervisor';//dado opcional
+}
+
+function redirecione(usuario: iUsuario){
+  if(usuario.cargo){
+   // redirecione(usuario.cargo);
+  }
+}
+
+
+
+  //if ('cargo' in usuario){ //verifica se a variavel possui o att
+    //redireciona para admin area
+  //}
+
+//Generic types
+
+function adicionaApendiceALista<T>(array: T[], value: T){
+  return array.map(() => value)
+}
+
+adicionaApendiceALista([1, 2, 3, 4], 2);
+
+
+const input = document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input', (event) =>{
+  const i = event.currentTarget as HTMLInputElement;
+  console.log(i.value);
+})
+
+
+ function soma(a: number, b:number){
   return a + b;
 }
-*/
+
 
 interface iAnimal {
   nome: string;
   tipo: 'terrestre' | 'aquático'; 
-  executarRugido(alturaDecibeis: number): void;  // quando for utilizar em uma variavel 
-}                                                // não podera usar um valor diferente do definido
-                                                 //como no exemplo abaixo.
+  domestico: boolean;         // quando for utilizar em uma variavel 
+}                             // não podera usar um valor diferente do definido
+                              //como no exemplo abaixo.
 
 interface iFelinos extends iAnimal {
   visaoNoturna: boolean;
 }
 
-const animal: iAnimal = {
-  nome: 'Elefante',
-  tipo: 'terrestre',
-  executarRugido: (alturaDecibeis) => (`${alturaDecibeis}dB`)
+interface iCanino extends iAnimal {
+  porte: 'pequeno' | 'medio' | 'grande';
 }
 
-const felino: iFelinos = {
-  nome: 'leao',
+type iDomestico = iFelinos | iCanino
+
+const animal: iDomestico = {
+  nome: 'Cachorro',
   tipo: 'terrestre',
-  executarRugido: (alturaDecibeis) => (`${alturaDecibeis}dB`),
+  porte:'medio',
+  domestico: true,
   visaoNoturna: true
 }
