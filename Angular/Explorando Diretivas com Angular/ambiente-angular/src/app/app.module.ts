c7c7171cb1fbf590fe3cdc5f35ac683ee06c8999
Henrique Sanches Silva <87503412+HenriqueSanchesSilva@users.noreply.github.com>
2022-05-09 12:15:49 -0300

    fim

---
+++ Angular/Explorando Diretivas com Angular/ambiente-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './directive/marca-texto.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MarcaTextoDirective,
+    
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
